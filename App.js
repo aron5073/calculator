c650a5fbc6ca201d199a2aef9ee97a02e51fb10e
5073akshay <akshay.sharma@appwrk.com>
2021-03-17 16:25:44 +0530

    working calculator with some bugs

--- App.js
+++ App.js
@@ -22,73 +22,173 @@ const styles = StyleSheet.create({
   },
   value: {
     color: '#fff',
-    fontSize: 40,
+    fontSize: 60,
     textAlign: 'right',
     marginRight: 20,
+    marginBottom: 20,
   },
 });
 
+const initialState = {
+  currentValue: '0',
+  operator: null,
+  previousValue: null,
+};
+
 export default class App extends React.Component {
+  state = initialState;
+
+  // type we contain whether its number or operators
+  handleTap = (type, value) => {
+    this.setState(state => {
+      if (type === 'number') {
+        if (state.currentValue === '0') {
+          return {currentValue: `${value}`};
+        }
+        return {
+          currentValue: `${state.currentValue}${value}`,
+        };
+      }
+      if (type === 'AC') {
+        return initialState;
+      }
+      if (type === 'posneg') {
+        return {
+          currentValue: `${parseFloat(state.currentValue) * -1}`,
+        };
+      }
+      if (type === 'percentage') {
+        return {
+          currentValue: `${parseFloat(state.currentValue) * 0.01}`,
+        };
+      }
+      if (type === 'operator') {
+        return {
+          operator: value,
+          previousValue: state.currentValue,
+          currentValue: '0',
+        };
+      }
+      if (type === 'equal') {
+        const {currentValue, previousValue, operator} = state;
+        const current = parseFloat(currentValue);
+        const previous = parseFloat(previousValue);
+        const resetState = {
+          operator: null,
+          previousValue: null,
+        };
+        if (operator === '/') {
+          return {
+            currentValue: previous / current,
+            ...resetState,
+          };
+        }
+        if (operator === '*') {
+          return {
+            currentValue: current * previous,
+            ...resetState,
+          };
+        }
+        if (operator === '+') {
+          return {
+            currentValue: current + previous,
+            ...resetState,
+          };
+        }
+        if (operator === '-') {
+          return {
+            currentValue: previous - current,
+            ...resetState,
+          };
+        }
+      }
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <SafeAreaView>
-          <Text style={styles.value}>75</Text>
+          <Text style={styles.value}>
+            {parseFloat(this.state.currentValue)
+              .toString()
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+          </Text>
           <Row>
             <Button
               text="AC"
               size="lightgrey"
-              onPress={() => alert('todo1')}></Button>
+              onPress={() => this.handleTap('AC', 'AC')}></Button>
             <Button
               text="+/-"
               size="lightgrey"
-              onPress={() => alert('todo2')}></Button>
+              onPress={() => this.handleTap('posneg', '')}></Button>
             <Button
               text="%"
               size="lightgrey"
-              onPress={() => alert('todo3')}></Button>
+              onPress={() => this.handleTap('percentage', '%')}></Button>
             <Button
               text="/"
               size="orange"
-              onPress={() => alert('todo4')}></Button>
+              onPress={() => this.handleTap('operator', '/')}></Button>
           </Row>
           <Row>
-            <Button text="7" onPress={() => alert('todo1')}></Button>
-            <Button text="8" onPress={() => alert('todo2')}></Button>
-            <Button text="9" onPress={() => alert('todo3')}></Button>
+            <Button
+              text="7"
+              onPress={() => this.handleTap('number', 7)}></Button>
+            <Button
+              text="8"
+              onPress={() => this.handleTap('number', 8)}></Button>
+            <Button
+              text="9"
+              onPress={() => this.handleTap('number', 9)}></Button>
             <Button
               text="x"
               size="orange"
-              onPress={() => alert('todo4')}></Button>
+              onPress={() => this.handleTap('operator', '*')}></Button>
           </Row>
           <Row>
-            <Button text="4" onPress={() => alert('todo1')}></Button>
-            <Button text="5" onPress={() => alert('todo2')}></Button>
-            <Button text="6" onPress={() => alert('todo3')}></Button>
+            <Button
+              text="4"
+              onPress={() => this.handleTap('number', 4)}></Button>
+            <Button
+              text="5"
+              onPress={() => this.handleTap('number', 5)}></Button>
+            <Button
+              text="6"
+              onPress={() => this.handleTap('number', 6)}></Button>
             <Button
               text="-"
               size="orange"
-              onPress={() => alert('todo4')}></Button>
+              onPress={() => this.handleTap('operator', '-')}></Button>
           </Row>
           <Row>
-            <Button text="1" onPress={() => alert('todo1')}></Button>
-            <Button text="2" onPress={() => alert('todo2')}></Button>
-            <Button text="3" onPress={() => alert('todo3')}></Button>
             <Button
-              text="*"
+              text="1"
+              onPress={() => this.handleTap('number', 1)}></Button>
+            <Button
+              text="2"
+              onPress={() => this.handleTap('number', 2)}></Button>
+            <Button
+              text="3"
+              onPress={() => this.handleTap('number', 3)}></Button>
+            <Button
+              text="+"
               size="orange"
-              onPress={() => alert('todo4')}></Button>
+              onPress={() => this.handleTap('operator', '+')}></Button>
           </Row>
           <Row>
             <Button
               text="0"
               size="double"
-              onPress={() => alert('todo1')}></Button>
-            <Button text="." onPress={() => alert('todo2')}></Button>
+              onPress={() => this.handleTap('number', 0)}></Button>
+            <Button
+              text="."
+              onPress={() => this.handleTap('number', '.')}></Button>
             <Button
               text="="
               size="orange"
-              onPress={() => alert('todo3')}></Button>
+              onPress={() => this.handleTap('equal', '=')}></Button>
           </Row>
         </SafeAreaView>
       </View>
